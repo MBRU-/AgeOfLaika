194f8d7323dd9fda697b0474d51687370b059ebb
MBRU- <mbru_mbrux@me.com>
2014-11-02 14:17:57 +0100

    Age of Laika with some input value testing
    calculate-button is hidden until value entered in text field
    calculating with doubles
    checking for empty field before calculation
    capturing „change messages“ from text entry field to update info on
    screen

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var enterNewAgeTextField: UITextField!
+    
+    @IBOutlet weak var calculateAgeLabel: UILabel!
+    @IBOutlet weak var calculateButtonRef: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        calculateAgeLabel.hidden = true
+        calculateButtonRef.hidden = true
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func calculateAgeButton(sender: UIButton) {
+
+        let dogAgeFactor = 7.0
+        if !enterNewAgeTextField.text.isEmpty {
+            let dateEntryInt = Double((enterNewAgeTextField.text as NSString).doubleValue)
+            enterNewAgeTextField.text = ""
+            calculateAgeLabel.text = "Dog's age of \(dateEntryInt * dogAgeFactor)"
+            calculateAgeLabel.hidden = false
+            
+        }
+        calculateButtonRef.hidden = true
+    }
+    
+    @IBAction func enterNewAgeTextFieldAction(sender: UITextField) {
+    if enterNewAgeTextField.text.isEmpty {
+        calculateButtonRef.hidden = true
+    } else {
+        
+        calculateButtonRef.hidden = false
+        calculateAgeLabel.text = ""
+        println("change in entry field")
+        }
+    }
 
 }
 
